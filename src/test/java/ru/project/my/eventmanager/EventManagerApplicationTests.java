1d79878dc60f08232b26543de21aa867ac706d32
a.pribytkov <jedi12@mail.ru>
2026-01-30 15:18:17 +0700

    Init commit

---
+++ src/test/java/ru/project/my/eventmanager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.my.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
